a1630019a6b26d8c07985b9c4954bdad7c840bcf
yamadayub <yamadayub@gmail.com>
2020-06-06 06:25:45 +0000

    heroku setting

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,8 @@ module Boo
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    config.web_console.whitelisted_ips = '124.209.183.109'
+    # unless Rails.env.production?
+    #   config.web_console.whitelisted_ips = '124.209.183.109'
+    # end
   end
 end
