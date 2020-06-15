05f005350e75d13a162b0842b61a4e83787a74b2
yamadayub <yamadayub@gmail.com>
2020-06-15 16:53:37 +0000

    twitter_username

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,7 +26,7 @@ class PostsController < ApplicationController
     @post.net_thumbs = 0
     @post.tweet_url = params[:post][:tweet_url]
     @post.like_in_twitter = params[:post][:like_in_twitter]
-    @post.twitter_usernane = params[:post][:username]
+    @post.twitter_username = params[:post][:username]
     #binding.pry
     
     if @post.save
