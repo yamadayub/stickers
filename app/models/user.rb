921a20b7a5594b2d1f62517f08603a6becf4d1fb
yamadayub <yamadayub@gmail.com>
2020-11-21 05:01:41 +0000

    remove user model

--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  before_save { self.email.downcase! }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
-  has_secure_password
-  has_many :dislikes
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_20_140650) do
+ActiveRecord::Schema.define(version: 2020_11_21_045614) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "sns_post_url"
@@ -26,13 +26,6 @@ ActiveRecord::Schema.define(version: 2020_06_20_140650) do
     t.integer "like_in_twitter"
     t.string "twitter_username"
     t.integer "total_net_like"
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
